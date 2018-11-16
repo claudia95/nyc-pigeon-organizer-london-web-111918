deebdde982cf8816e9d0d283cfac7f87d6be3e11
Claudia Fox <claudiafox95@icloud.com>
2018-11-16 17:20:09 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,19 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |attribute, value|
-    value.each do |info, pigeon_names|
+  pigeons = {}
+  data.each do |attribute, values|
+    values.each do |info, pigeon_names|
       pigeon_names.each do |name|
-        if pigeon_list.include?(name)
-          if pigeon_list[name].include?(attribute)
-            pigeon_list[name][attribute] << info.to_s
+        if pigeons.include?(name)
+          if pigeons[name].include?(attribute)
+            pigeons[name][attribute] << info.to_s
           else
-            pigeon_list[name][attribute] = [info.to_s]
+            pigeons[name][attribute] = [info.to_s]
           end
         else
-          pigeon_list[name] = {attribute => [info.to_s]}
+          pigeons[name] = {attribute => [info.to_s]}
         end
       end
     end
   end
-  pigeon_list
+  pigeons
 end
